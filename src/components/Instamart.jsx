5707c8765479e19050f2a5f66e595abbaf945fdb
adnan2212 <adnanshaikh2607@gmail.com>
2023-05-08 22:17:14 +0530

    Added accordian

--- src/components/Instamart.jsx
+++ src/components/Instamart.jsx
@@ -1,8 +1,76 @@
-const Instamart = () => {
+import { useState } from "react";
+
+const Section = ({ title, description, isVisible, setIsVisible }) => {
   return (
-    <div style={{ padding: 250 }}>
-      <div>Instamart</div>
-      <h1>100s of Components</h1>
+    <div
+      style={{
+        // display: "flex",
+        border: "1px solid black",
+        padding: 15,
+        paddingBottom: 6,
+        margin: 10,
+      }}
+    >
+      <h1>{title}</h1>
+
+      <button onClick={() => setIsVisible(!isVisible)}>
+        {/* <span>
+          {isVisible ? (
+            <i className="fa-solid fa-arrow-up"></i>
+          ) : (
+            <i className="fa-solid fa-arrow-down"></i>
+          )}
+        </span> */}
+        <span>{isVisible ? <p>Hide</p> : <p>Show</p>}</span>
+      </button>
+      {isVisible && <p>{description}</p>}
+    </div>
+  );
+};
+
+const Instamart = () => {
+  const [visibleSection, setIsVisibleSection] = useState("aboutInstamart");
+
+  return (
+    <div style={{ padding: 170 }}>
+      <Section
+        title={"About Instamart"}
+        description={
+          "At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga. Et harum quidem rerum facilis est et expedita distinctio. Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil impedit quo minus id quod maxime placeat facere possimus, omnis voluptas assumenda est, omnis dolor repellendus. Temporibus autem quibusdam et aut officiis debitis aut rerum necessitatibus saepe eveniet ut et voluptates repudiandae sint et molestiae non recusandae. Itaque earum rerum hic tenetur a sapiente delectus, ut aut reiciendis voluptatibus maiores alias consequatur aut perferendis doloribus asperiores repellat."
+        }
+        isVisible={visibleSection === "aboutInstamart"}
+        setIsVisible={() =>
+          setIsVisibleSection(
+            visibleSection === "aboutInstamart" ? "" : "aboutInstamart"
+          )
+        }
+      />
+      <Section
+        title={"Team Instamart"}
+        description={
+          "At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga. Et harum quidem rerum facilis est et expedita distinctio. Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil impedit quo minus id quod maxime placeat facere possimus, omnis voluptas assumenda est, omnis dolor repellendus. Temporibus autem quibusdam et aut officiis debitis aut rerum necessitatibus saepe eveniet ut et voluptates repudiandae sint et molestiae non recusandae. Itaque earum rerum hic tenetur a sapiente delectus, ut aut reiciendis voluptatibus maiores alias consequatur aut perferendis doloribus asperiores repellat."
+        }
+        isVisible={visibleSection === "teamInstamart"}
+        setIsVisible={() =>
+          setIsVisibleSection(
+            visibleSection === "teamInstamart" ? "" : "teamInstamart"
+          )
+        }
+      />
+
+      <Section
+        title={"Careers at Instamart"}
+        description={
+          "At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga. Et harum quidem rerum facilis est et expedita distinctio. Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil impedit quo minus id quod maxime placeat facere possimus, omnis voluptas assumenda est, omnis dolor repellendus. Temporibus autem quibusdam et aut officiis debitis aut rerum necessitatibus saepe eveniet ut et voluptates repudiandae sint et molestiae non recusandae. Itaque earum rerum hic tenetur a sapiente delectus, ut aut reiciendis voluptatibus maiores alias consequatur aut perferendis doloribus asperiores repellat."
+        }
+        isVisible={visibleSection === "career"}
+        setIsVisible={() =>
+          setIsVisibleSection(visibleSection === "career" ? "" : "career")
+        }
+      />
+
+      {/* <div>Instamart</div>
+      <h1>100s of Components</h1> */}
     </div>
   );
 };
